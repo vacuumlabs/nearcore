22a524d1409b69147e072cf65281dbf4827c69e9
Nikolay Igotti <igotti@gmail.com>
2021-02-20 10:38:06 +0300

    Refactor

--- runtime/near-vm-runner/src/lib.rs
+++ runtime/near-vm-runner/src/lib.rs
@@ -7,15 +7,17 @@ mod runner;
 mod wasmer_runner;
 #[cfg(feature = "wasmtime_vm")]
 mod wasmtime_runner;
+mod preload;
+
 pub use near_vm_errors::VMError;
 pub use runner::compile_module;
-pub use runner::prepare_vm_calls;
 pub use runner::run;
-pub use runner::run_prepared;
 pub use runner::run_vm;
 pub use runner::run_vm_profiled;
 pub use runner::with_vm_variants;
-pub use runner::{ContractCallPrepareRequest, ContractCallPrepareResult, ContractCallContext};
+pub use preload::preload_vm_calls;
+pub use preload::run_preloaded;
+pub use preload::{ContractCallPrepareRequest, ContractCallPrepareResult, ContractCallContext};
 
 #[cfg(feature = "costs_counting")]
 pub use near_vm_logic::EXT_COSTS_COUNTER;
--- runtime/near-vm-runner/src/runner.rs
+++ runtime/near-vm-runner/src/runner.rs
@@ -8,70 +8,6 @@ use near_vm_logic::types::PromiseResult;
 use near_vm_logic::{External, VMContext, VMKind, VMOutcome};
 use std::sync::Arc;
 use borsh::maybestd::sync::RwLock;
-use threadpool::ThreadPool;
-
-#[derive(Debug)]
-struct CallInner {
-    // smth here
-    foo: i32,
-}
-
-#[derive(Debug, Clone)]
-pub struct ContractCallPrepareRequest {
-    pub code_hash: CryptoHash,
-    pub code: Vec<u8>,
-    pub method_name: Vec<u8>,
-}
-
-#[derive(Debug)]
-pub struct ContractCallContext {
-    prepared: Vec<CallInner>,
-    pool: ThreadPool,
-}
-
-#[derive(Debug, Clone)]
-struct ExecRequest {
-    context: Arc<RwLock<ContractCallContext>>,
-    request: ContractCallPrepareRequest,
-    index: u32,
-}
-
-unsafe impl Send for ContractCallPrepareRequest {}
-unsafe impl Send for ExecRequest {}
-
-impl ContractCallContext {
-    pub fn new(num_threads: usize) -> Arc<RwLock<ContractCallContext>> {
-        Arc::new(RwLock::new(ContractCallContext {
-            prepared: Vec::new(),
-            pool: ThreadPool::new(num_threads),
-        }))
-    }
-
-    pub fn schedule(self: &mut ContractCallContext,
-                    context: Arc<RwLock<ContractCallContext>>,
-                    request: ContractCallPrepareRequest, index: u32) {
-        let copy_request = request.clone();
-        self.pool.execute(move || {
-            prepare_in_thread(index, copy_request);
-        });
-    }
-}
-
-impl Drop for ContractCallContext {
-    fn drop(&mut self) {
-        println!("Closing call context");
-    }
-}
-
-pub struct ContractCallPrepareResult {
-    pub context: Arc<RwLock<ContractCallContext>>,
-    pub handle: Option<u32>,
-    pub error: Option<VMError>,
-}
-
-fn prepare_in_thread(index: u32, request: ContractCallPrepareRequest) {
-    println!("{} exec on {:?}", index, request.code_hash);
-}
 
 /// `run` does the following:
 /// - deserializes and validate the `code` binary (see `prepare::prepare_contract`)
@@ -286,46 +222,4 @@ pub fn compile_module(vm_kind: VMKind, code: &Vec<u8>) -> bool {
         }
     };
     false
-}
-
-pub fn prepare_vm_calls<'a>(
-    call_context: Arc<RwLock<ContractCallContext>>,
-    requests: Vec<ContractCallPrepareRequest>,
-    cache: Option<Arc<RwLock<dyn CompiledContractCache>>>,
-    vm_config: &VMConfig,
-    vm_kind: VMKind,
-) -> Vec<ContractCallPrepareResult> {
-    let mut result: Vec<ContractCallPrepareResult> = Vec::new();
-    let mut mut_context = call_context.write().unwrap();
-    for request in requests {
-        let index = mut_context.prepared.len() as u32;
-        mut_context.prepared.push(CallInner { foo: 42 });
-        mut_context.schedule(call_context.clone(), request, index);
-        result.push(ContractCallPrepareResult { context: call_context.clone(), handle: Some(index), error: None });
-    }
-    result
-}
-
-pub fn run_prepared<'a>(
-    call_context: Arc<RwLock<ContractCallContext>>,
-    prepared: &ContractCallPrepareResult,
-    ext: &mut dyn External,
-    context: VMContext,
-    vm_config: &'a VMConfig,
-    fees_config: &'a RuntimeFeesConfig,
-    promise_results: &'a [PromiseResult],
-    current_protocol_version: ProtocolVersion,
-    profile: Option<&ProfileData>,
-) -> (Option<VMOutcome>, Option<VMError>) {
-    match &prepared.error {
-        Some(error) => return (None, Some(error.clone())),
-        _ => {}
-    }
-    match prepared.handle {
-        Some(handle) => {
-            println!("handle is {}", handle);
-        }
-        None => panic!("Must be valid"),
-    }
-    (None, None)
 }
